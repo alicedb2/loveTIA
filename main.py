4f648b796defc1be03df933994b7dedc58bf3e71
Alice Doucet Beaupré <alice.doucet.beaupre@gmail.com>
2022-11-05 17:56:31 -0400

    clock divisor to control pitch continuously

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ from machine import Pin, freq, mem32, ADC
 ADDR_CTRL0 = 0b0101
 ADDR_CTRL1 = 0b0110
 # A2600 pitch addresse
-# Allow to set divisors of 
+# Allow to set divisors of
 # a fundamental frequency
+# (something between 30-32kHz, not sure)
 ADDR_FREQ0 = 0b0111
 ADDR_FREQ1 = 0b1000
 # A2600 volume addresses
@@ -21,18 +22,20 @@ unique_waveforms = (1, 2, 3, 4, 6, 7, 8, 12, 14, 15)
 tia_rw_pin = Pin(5, Pin.OUT, value=0)
 addr_pins = [Pin(i, Pin.OUT, value=0) for i in (6, 7, 8, 9)]
 data_pins = [Pin(i, Pin.OUT, value=0) for i in (10, 11, 12, 13, 14)]
-adc_waveform = ADC(Pin(28))
-adc_pitch = ADC(Pin(27))
-adc_volume = ADC(Pin(26))
+
+adc_waveform = ADC(Pin(27))
+adc_pitch = ADC(Pin(26))
+adc_divisor = ADC(Pin(28))
+#adc_volume = ADC(Pin())
 
 ##########################################################
 ## Start oscillator to original Atari 2600 NTSC 3.58MHz ##
 ##########################################################
-# This calculates the required state machine 
+# This calculates the required state machine
 # clock division given a target clock frequency
 def sm_div_calc(target_f):
-    # We multiply by two because the PIO
-    # state machine is made of 2 instructions
+    # We multiply by two because the PIOX
+    # state machine contains 2 instructions
     target_f *= 2
     if target_f < 0:
         division = 256
@@ -45,6 +48,7 @@ def sm_div_calc(target_f):
             raise ValueError("freq out of range")
     return division << 8
 
+
 # A2600 NTSC crystal frequency
 ATARI_FREQ = 3_579_545
 
@@ -53,7 +57,8 @@ ATARI_FREQ = 3_579_545
 # the clock division of state machine 0
 # on the fly without having to restart
 # the state machine
-ADDR_SM0_CLKDIV = 0x50200000 + 0x0c8
+ADDR_SM0_CLKDIV = 0x50200000 + 0x0C8
+
 
 @rp2.asm_pio(set_init=rp2.PIO.OUT_LOW)
 def squarewave():
@@ -62,54 +67,83 @@ def squarewave():
 
 sm0 = rp2.StateMachine(0, squarewave, freq=freq(), set_base=Pin(17))
 sm0.active(1)
-mem32[ADDR_SM0_CLKDIV] = sm_div_calc(ATARI_FREQ)
+
+
+def set_clock_freq(target_freq=ATARI_FREQ):
+    mem32[ADDR_SM0_CLKDIV] = sm_div_calc(int(target_freq))
+
 
 ##########################################################
+
 
 def loop(voice=2):
     last_volume_val = -1
     last_pitch_val = -1
+    last_divisor_val = -1
     last_waveform_val = -1
 
+    setVolume(15, voice)
+
     while True:
-        
+        sleep(0.05)
+
         # Volume
-        pot_read = oversampled_read(adc_volume, 4)
-        volume_val = map2bin(pot_read, 0, 65535, 16)
-        if volume_val != last_volume_val:
-            setVolume(voice, volume_val)
-            last_volume_val = volume_val
+        #pot_read = oversampled_read(adc_volume, 4)
+        #volume_val = binval(pot_read, 300, 60000, 16)
+        #if volume_val != last_volume_val:
+        #    setVolume(volume_val, voice)
+        #    last_volume_val = volume_val
+        #    print(f"volume_{volume_val} pot_{pot_read} div_{divisor}")
 
         # Pitch
         pot_read = oversampled_read(adc_pitch, 4)
-        pitch_val = map2bin(pot_read, 0, 65535, 32)
+        pitch_val = binval(pot_read, 300, 60000, 32)
         if pitch_val != last_pitch_val:
-            setPitch(voice, pitch_val)
+            setPitch(pitch_val + 1, voice)
             last_pitch_val = pitch_val
+            # print(f"pitch_{pitch_val} pot_{pot_read}")
+
+        # Clock divisor
+        divisor_val = oversampled_read(adc_divisor, 4)
+        if divisor_val != last_divisor_val:
+            divisor = map(divisor_val, 300, 60000, 1, 128)
+            set_clock_freq(ATARI_FREQ/divisor)
+            last_divisor_val = divisor_val
+            # print(f"potread_{divisor_val} divisor_{divisor}")
+
 
         # Waveform
         pot_read = oversampled_read(adc_waveform, 4)
-        waveform_val = map2bin(pot_read, 0, 65535, 10)
+        waveform_val = binval(pot_read, 300, 60000, 10)
         if waveform_val != last_waveform_val:
-            setWaveform(voice, waveform_val)
+            setWaveform(waveform_val, voice)
             last_waveform_val = waveform_val
+            # print(f"waveform_{waveform_val} pot_{pot_read}")
 
 
-        
 def map(value, input_min, input_max, output_min, output_max):
-     out_val = (value - input_min) * (output_max - output_min) / (input_max - input_min) + output_min
-     return int(out_val)
 
-def map2bin(value, input_min, input_max, nb_bins):
-    out_val = map(value, input_min, input_max + 1, 0, nb_bins)
+    if value < input_min: return output_min
+    if value > input_max: return output_max
+
+    out_val = (value - input_min) * (output_max - output_min) / (
+        input_max - input_min
+    ) + output_min
     return out_val
+
+
+def binval(value, input_min, input_max, nb_bins):
+    bin_width = (input_max - input_min) / nb_bins
+    return max(0, min(nb_bins - 1, int((value - input_min) / bin_width)))
+
 
 def oversampled_read(adc, nb_oversampling_bits):
     pot_read = 0
-    for k in range(2**nb_oversampling_bits):
+    for k in range(2 ** nb_oversampling_bits):
         pot_read += adc.read_u16()
     pot_read >>= nb_oversampling_bits
     return pot_read
+
 
 def setTIApins(address, data):
     for bit, pin in enumerate(addr_pins):
@@ -117,37 +151,66 @@ def setTIApins(address, data):
     for bit, pin in enumerate(data_pins):
         pin.value(data >> bit & 1)
 
+
 # volume from 0b0000 = 0 to 0b1111 = 15
-def setVolume(voice, volume):
+def setVolume(volume, voice=2):
+
+    volume = int(volume)
+
+    if volume < 0 or volume > 15:
+        print("Volume values should be between 0 and 15")
+        return
+
     if voice == 0 or voice == 2:
         tia_rw_pin.high()
         setTIApins(ADDR_VOL0, volume)
         tia_rw_pin.low()
+    sleep(0.001)
     if voice == 1 or voice == 2:
         tia_rw_pin.high()
         setTIApins(ADDR_VOL1, volume)
         tia_rw_pin.low()
 
+
 # raw waveform from 0b0000 = 0 to 0b1111 = 15
 # unique waveforms from 0 to 10
-def setWaveform(voice, waveform):
+def setWaveform(waveform, voice=2):
+
+    waveform = int(waveform)
+
+    if waveform < 0 or waveform > 9:
+        print("Waveform values should be between 0 and 9")
+        return
+
     if voice == 0 or voice == 2:
         tia_rw_pin.high()
         setTIApins(ADDR_CTRL0, unique_waveforms[waveform])
         tia_rw_pin.low()
+        sleep(0.001)
     if voice == 1 or voice == 2:
         tia_rw_pin.high()
         setTIApins(ADDR_CTRL1, unique_waveforms[waveform])
         tia_rw_pin.low()
 
+
 # divisor from 0b00000 = 1 to 0b11111 = 32
 # divides fundamental frequency ~30kHz
-def setPitch(voice, divisor):
+def setPitch(divisor, voice=2):
+
+    divisor = int(divisor)
+
+    if divisor < 1 or divisor > 32:
+        print("Divisor values should be between 1 and 32")
+        return
+
     if voice == 0 or voice == 2:
         tia_rw_pin.high()
-        setTIApins(ADDR_FREQ0, divisor)
+        setTIApins(ADDR_FREQ0, divisor - 1)
         tia_rw_pin.low()
+    sleep(0.001)
     if voice == 1 or voice == 2:
         tia_rw_pin.high()
-        setTIApins(ADDR_FREQ1, divisor)
+        setTIApins(ADDR_FREQ1, divisor - 1)
         tia_rw_pin.low()
+
+
